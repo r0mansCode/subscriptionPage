451be4714957f24f6734073a64b54f685e9c97bc
r0mansCode <kovaljonokroman@gmail.com>
2021-12-13 21:14:21 +0200

    changing logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './Navbar.scss';
-import { GiPineapple } from 'react-icons/gi';
+import Union from '../../Assets/Union.png';
+// import { GiPineapple } from 'react-icons/gi';
 
 export class Navbar extends Component {
     render() {
         return (
             <nav className='navBar'>
                 <div className='navBar__text'>
-                <div className='navBar__text__elPineapple'> <GiPineapple /> pineapple.</div> 
+                <div className='navBar__text__elPineapple'> <img src={Union} /> pineapple.</div> 
                 <div className='navBar__text__el'>About</div>
                 <div className='navBar__text__el'>How it works</div>
                 <div className='navBar__text__el'>Contact</div>

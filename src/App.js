5bd38b76d4e5137763591661d830431227bd1c7d
r0mansCode <kovaljonokroman@gmail.com>
2021-12-14 20:56:06 +0200

    implement routing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.scss';
 import Navbar from './Components/Navbar/Navbar';
 import Main from './Components/Main/Main';
 import Background from './Components/Background/Background';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <Router >
     <div className='app'>
       <Navbar/>
       <Main />
       <Background />
     </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Navbar.scss';
 import Union from '../../Assets/Union.png';
+import { Link } from 'react-router-dom';
 
 export class Navbar extends Component {
     render() {
@@ -8,12 +9,12 @@ export class Navbar extends Component {
             <nav className='navBar'>
                 <div className='navBar__text'>
                 <div className='navBar__text__elPineapple'> 
-                    <img src={Union} className='navBar__text__elPineapple__elPicContainer' alt="pineapple" />
+                    <Link to="/"><img src={Union} className='navBar__text__elPineapple__elPicContainer' alt="pineapple" /></Link>
                     <div className='navBar__text__elPineapple__elText'>pineapple.</div>
                 </div>
-                <div className='navBar__text__el'>About</div>
-                <div className='navBar__text__el'>How it works</div>
-                <div className='navBar__text__el'>Contact</div>
+                <Link className='navBar__text__el' to="/about">About</Link>
+                <Link className='navBar__text__el' to="/how">How it works</Link>
+                <Link className='navBar__text__el' to="/contact">Contact</Link>
                 </div>
             </nav>
         )
